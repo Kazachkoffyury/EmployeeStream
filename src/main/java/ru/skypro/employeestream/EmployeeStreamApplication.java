a84c66545759ff17b616dc5de55a0a44465d6cbb
Kazackoffyury <kazachkoff.yury@yandex.ru>
2021-12-20 22:05:52 +0300

    initialize

---
+++ src/main/java/ru/skypro/employeestream/EmployeeStreamApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/employeestream/EmployeeStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
