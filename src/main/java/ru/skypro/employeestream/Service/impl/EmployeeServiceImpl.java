cb28a234e2d5ac0b1c82df9ff30e8fd05240ce11
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-01-06 16:39:40 +0300

    fix#1

--- src/main/java/ru/skypro/employeestream/Service/impl/EmployeeServiceImpl.java
+++ src/main/java/ru/skypro/employeestream/Service/impl/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import ru.skypro.employeestream.Data.Employee;
 import ru.skypro.employeestream.Service.EmployeeService;
 import ru.skypro.employeestream.exception.DuplicateEmployee;
 import ru.skypro.employeestream.exception.EmployeeNotFound;
+import ru.skypro.employeestream.exception.ErrorString;
 
 
 import java.util.Collection;
@@ -22,21 +23,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
     @Override
     public Employee addEmployee(String firstName, String lastName, int department, int salary) throws DuplicateEmployee {
-
-        if(!employees.containsKey(getKeyString(firstName,lastName))) {
-            Employee employee = new Employee(firstName,lastName,department,salary);
-            if(StringUtils.isAlpha(employee.getFullName())) {
+        if (isNumberString(firstName) && isNumberString(lastName)) {
+            if(!employees.containsKey(getKeyString(firstName,lastName))) {
+                Employee employee = new Employee(firstName,lastName,department,salary);
                 employee.setFirstName(StringUtils.capitalize(firstName));
                 employee.setLastName(StringUtils.capitalize(lastName));
                 employees.put(employee.getFullName(), employee);
                 return employee;
-            }
-            throw  new EmployeeNotFound();
+                }
+
+            throw new DuplicateEmployee();
+
         }
+        throw  new ErrorString();
+    }
 
-        throw new DuplicateEmployee();
 
-   }
+
 
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
@@ -49,7 +52,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee getEmployee(String firstName, String lastName) {
-        return null;
+        if (isNumberString(firstName) && isNumberString(lastName)) {
+            return  employees.get(getKeyString(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName)));
+        }
+        throw  new ErrorString();
+
     }
 
     @Override
@@ -59,5 +66,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private String getKeyString(String firstName, String lastName) {
         return  firstName+" "+lastName;
+    }
+
+    public boolean isNumberString(String str) {
+        if (StringUtils.isAlpha(str)){
+            return true;
+        }
+       else throw new ErrorString();
+
     }
 }
