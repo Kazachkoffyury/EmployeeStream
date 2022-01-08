76fa2db99a83605f8dddc01aa19635aa6eee513a
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-01-08 13:58:44 +0300

    fix#2

--- src/main/java/ru/skypro/employeestream/Service/impl/EmployeeServiceImpl.java
+++ src/main/java/ru/skypro/employeestream/Service/impl/EmployeeServiceImpl.java
@@ -6,7 +6,7 @@ import ru.skypro.employeestream.Data.Employee;
 import ru.skypro.employeestream.Service.EmployeeService;
 import ru.skypro.employeestream.exception.DuplicateEmployee;
 import ru.skypro.employeestream.exception.EmployeeNotFound;
-import ru.skypro.employeestream.exception.ErrorString;
+import ru.skypro.employeestream.exception.WrongEmployeeNameException;
 
 
 import java.util.Collection;
@@ -24,18 +24,19 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee addEmployee(String firstName, String lastName, int department, int salary) throws DuplicateEmployee {
         if (isNumberString(firstName) && isNumberString(lastName)) {
-            if(!employees.containsKey(getKeyString(firstName,lastName))) {
-                Employee employee = new Employee(firstName,lastName,department,salary);
-                employee.setFirstName(StringUtils.capitalize(firstName));
-                employee.setLastName(StringUtils.capitalize(lastName));
-                employees.put(employee.getFullName(), employee);
-                return employee;
-                }
-
-            throw new DuplicateEmployee();
 
         }
-        throw  new ErrorString();
+        else throw new WrongEmployeeNameException();
+
+        if(!employees.containsKey(getKeyString(firstName,lastName))) {
+            Employee employee = new Employee(firstName,lastName,department,salary);
+            employee.setFirstName(StringUtils.capitalize(firstName));
+            employee.setLastName(StringUtils.capitalize(lastName));
+            employees.put(employee.getFullName(), employee);
+            return employee;
+        }
+
+        throw new DuplicateEmployee();
     }
 
 
@@ -55,7 +56,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (isNumberString(firstName) && isNumberString(lastName)) {
             return  employees.get(getKeyString(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName)));
         }
-        throw  new ErrorString();
+        throw  new WrongEmployeeNameException();
 
     }
 
@@ -72,7 +73,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (StringUtils.isAlpha(str)){
             return true;
         }
-       else throw new ErrorString();
+       else throw new WrongEmployeeNameException();
 
     }
 }
